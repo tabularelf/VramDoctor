7758803cd392a7535dc974158772b6e153f9fbfc
tabularelf <tabularelf@gmail.com>
2024-05-18 02:16:26 +1000

    Updated version

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
 	name: 'Vram Doctor',
 	description: 'Vram memory usage estimation for GameMaker',
-	latestVersion: '1.0.0',
-	otherVersions: ['1.0.0'],
+	latestVersion: '1.0.1',
+	otherVersions: ['1.0.1'],
 	favicon: 'assets/favicon.ico',
 };
